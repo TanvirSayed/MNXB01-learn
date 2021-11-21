806022f6f143952c9430172d63cff7285b47f50e
TerrificTanvir <72144187+TanvirSayed@users.noreply.github.com>
2021-11-21 14:48:57 +0100

    Add files via upload

---
+++ HW4-final/Tanvirhomework4.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH -J "hw4"
+#SBATCH --time=00:05:00 
+#SBATCH -A lu2021-7-19
+
+#echo "CPU info:"
+#cat /proc/cpuinfo
+#echo "memory details:"
+#cat /proc/meminfo
+echo "user:"
+whoami
+echo "current directory:"
+pwd
+echo 'hostname:'
+hostname
+echo 'uptime info:'
+uptime
+echo 'disk space usage:'
+du -h
+echo 'No of entries in /etc: '
+ls /etc | wc -l
+echo 'No of entries in /usr/bin directories'
+ls /usr/bin | wc -l 
+
+sleep 3m
